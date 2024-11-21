eb9783bc946403ce9dea4ee237e2e7b2764c9a2c
Thijs Van Den Berg <thivan-d@f1r5s16.codam.nl>
2024-11-21 19:41:38 +0100

    always forget something

---
+++ src/render/safe2file.c
@@ -0,0 +1,26 @@
+#include "../../includes/minirt.h"
+// #include <stdio.h>
+// #include <stdlib.h>
+
+void save_image_to_file(const unsigned char* raw_image_data, unsigned width, unsigned height, const char* filename) {
+    unsigned char* png_data = NULL; // Buffer for PNG data
+    size_t png_size = 0;           // Size of PNG data
+
+    // Encode the image to PNG format
+    unsigned error = lodepng_encode32(&png_data, &png_size, raw_image_data, width, height);
+    if (error) {
+        printf("Error %u: %s\n", error, lodepng_error_text(error));
+        return;
+    }
+
+    // Save the PNG data to a file
+    error = lodepng_save_file(png_data, png_size, filename);
+    if (error) {
+        printf("Error %u: %s\n", error, lodepng_error_text(error));
+    } else {
+        printf("Image saved to %s\n", filename);
+    }
+
+    // Free the PNG data buffer
+    free(png_data);
+}
