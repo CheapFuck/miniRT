989a2b44f2f8a96ce46577752fb74e8d980f6b87
Thijs Van Den Berg <thivan-d@f1r6s15.codam.nl>
2025-01-12 17:26:45 +0100

    test

--- src/render/render.c
+++ src/render/render.c
@@ -26,19 +26,6 @@ typedef struct s_thread_data {
     int num_threads;
 } t_thread_data;
 
-
-// typedef struct s_material_params
-// {
-//     t_hit_record    *hit;
-//     t_ray           ray;
-//     t_vector        normal;
-//     t_scene         *scene;
-//     t_color         base_color;
-//     int             depth;
-// }   t_material_params;
-
-
-// Helper function to calculate reflection ray
 t_ray get_reflection_ray(t_vector hit_point, t_vector normal, t_ray incident_ray)
 {
     t_ray reflection;
@@ -48,13 +35,14 @@ t_ray get_reflection_ray(t_vector hit_point, t_vector normal, t_ray incident_ray
     double dot_product = dot(incident_ray.direction, normal);
     t_vector reflection_dir = subtract(incident_ray.direction, 
                                      multiply_scalar(normal, 2.0 * dot_product));
-    
     reflection.direction = normalize(reflection_dir);
     return reflection;
 }
+
 t_color uint32_to_t_color(uint32_t color)
 {
-    return (t_color){
+    return (t_color)
+	{
         .r = (color >> 24) & 0xFF,
         .g = (color >> 16) & 0xFF,
         .b = (color >> 8) & 0xFF
@@ -63,7 +51,8 @@ t_color uint32_to_t_color(uint32_t color)
 
 t_color blend_colors(t_color original_color, t_color reflected_color, float reflectivity)
 {
-    return (t_color) {
+    return (t_color)
+	{
         .r = reflectivity * reflected_color.r + (1 - reflectivity) * original_color.r,
         .g = reflectivity * reflected_color.g + (1 - reflectivity) * original_color.g,
         .b = reflectivity * reflected_color.b + (1 - reflectivity) * original_color.b
@@ -75,11 +64,10 @@ t_vector refract(t_vector incident, t_vector normal, float eta_ratio)
 {
     double cos_theta = fmin(dot(multiply_scalar(incident, -1.0), normal), 1.0);
     double sin_theta2 = eta_ratio * eta_ratio * (1.0 - cos_theta * cos_theta);
-    // Check for total internal reflection
-    if (sin_theta2 > 1.0) {
-        return reflect(incident, normal); // Return reflection if TIR occurs
+    if (sin_theta2 > 1.0)
+	{
+        return reflect(incident, normal);
     }
-    
     t_vector r_out_perp = multiply_scalar(add(incident, multiply_scalar(normal, cos_theta)), eta_ratio);
     t_vector r_out_parallel = multiply_scalar(normal, -sqrt(fabs(1.0 - dot(r_out_perp, r_out_perp))));
     return add(r_out_perp, r_out_parallel);
@@ -87,23 +75,24 @@ t_vector refract(t_vector incident, t_vector normal, float eta_ratio)
 
 t_vector get_cylinder_normal(t_vector hit_point, t_cylinder *cylinder)
 {
-    // Project hit point onto cylinder axis
-    t_vector axis = normalize(cylinder->orientation);
-    t_vector cp = subtract(hit_point, cylinder->center);
-    double dot_prod = dot(cp, axis);
-    
-    // Point on axis closest to hit point
-    t_vector axis_point = add(cylinder->center, multiply_scalar(axis, dot_prod));
-    
-    // Normal is from axis to hit point
-    t_vector normal = normalize(subtract(hit_point, axis_point));
-    
-    return normal;
+	t_vector axis;
+	t_vector cp;
+	t_vector axis_point;
+	t_vector normal;
+	double dot_prod;
+
+    axis = normalize(cylinder->orientation);
+    cp = subtract(hit_point, cylinder->center);
+    dot_prod = dot(cp, axis);
+    axis_point = add(cylinder->center, multiply_scalar(axis, dot_prod));
+    normal = normalize(subtract(hit_point, axis_point));
+    return (normal);
 }
 t_vector reflect(t_vector direction, t_vector normal)
 {
-    double dot_product = dot(direction, normal);
-    return subtract(direction, multiply_scalar(normal, 2.0 * dot_product));
+    double dot_product;
+	dot_product = dot(direction, normal);
+    return (subtract(direction, multiply_scalar(normal, 2.0 * dot_product)));
 }
 
 
@@ -155,203 +144,6 @@ int is_cylinder_checkerboard(t_vector point, t_cylinder *cylinder, double scale)
     return (u_check + v_check) % 2;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-// Modified color calculation function with reflection support
-// t_color trace_ray(t_ray ray, t_scene *scene, int depth)
-// {
-//     t_hit_record hit;
-
-//     if (depth > MAX_REFLECTION_DEPTH)
-//         return ((t_color){0, 0, 0});
-//     t_color final_color = {0, 0, 0};
-//     t_vector normal;
-//     t_color black = {255, 255, 255};
-//     t_color white = {0, 0, 0};
-//     int i;
-//     hit.index = -1;
-//     hit.t = INFINITY;
-//     hit.hit = 0;
-//     hit.hit_from_inside = 0;
-//     i = 0;
-//     while (i < scene->num_spheres)
-//     {
-//         double t_sphere;
-//         ray.direction = normalize(ray.direction);
-//         if (intersect_sphere(&ray, &scene->spheres[i], &t_sphere) && t_sphere < hit.t)
-//         {
-//             hit.hit = 1;
-//             hit.t = t_sphere;
-//             hit.type = SPHERE;
-//             hit.index = i;
-//         }
-//         i++;
-//     }
-//     i = 0;
-//     while (i < scene->num_cylinders)
-//     {
-//         double t_cy;
-//         if (intersect_cylinder(&ray, &scene->cylinders[i], &t_cy) && t_cy < hit.t)
-//         {
-//             hit.hit = 1;
-//             hit.t = t_cy;
-//             hit.type = CYLINDER;
-//             hit.index = i;
-//         }
-//         i++;
-//     }
-//     i = 0;
-//     while (i < scene->num_discs)
-//     {
-//         double t_disc;
-//         if (intersect_disc(&ray, &scene->discs[i], &t_disc) && t_disc < hit.t)
-//         {
-//             hit.hit = 1;
-//             hit.t = t_disc;
-//             hit.type = DISC;
-//             hit.index = i;
-//         }
-//         i++;
-//     }
-//     i = 0;
-//     while (i < scene->num_planes)
-//     {
-//         double t_plane;
-//         if (intersect_plane(&ray, &scene->planes[i], &t_plane) && t_plane < hit.t)
-//         {
-//             hit.hit = 1;
-//             hit.t = t_plane;
-//             hit.type = PLANE;
-//             hit.index = i;
-//         }
-//         i++;
-//     }
-//     if (hit.hit)
-//     {
-//         hit.point = add(ray.origin, multiply_scalar(ray.direction, hit.t));
-//         switch (hit.type)
-//         {
-//             case SPHERE:
-//             {
-//                 t_sphere *sphere = &scene->spheres[hit.index];
-//                 normal = normalize(subtract(hit.point, sphere->center));
-//                 if (sphere->material.checker == 1)
-//                 {
-//                     t_vector local_point = subtract(hit.point, sphere->center);
-//                     double u = 2.0 + atan2(local_point.z, local_point.x) / (2 * M_PI);
-//                     double v = 2.0 - asin(local_point.y / sphere->radius) / M_PI;
-//                     int check_u = (int)(u * 20.0) % 2;
-//                     int check_v = (int)(v * 20.0) % 2;
-//                     t_color object_color = (check_u == check_v) ? white : black;
-//                     final_color = apply_lighting(hit.point, normal, object_color, scene, depth + 1);
-//                 }
-//                 else
-//                     final_color = apply_lighting(hit.point, normal, sphere->material.color, scene, depth + 1);
-//                 hit.material.reflectivity = sphere->material.reflectivity;
-//                 hit.material.transparency = sphere->material.transparency;
-//                 hit.material.refractive_index = sphere->material.refractive_index;
-//                 break;
-//             }
-//             case CYLINDER:
-//             {
-//                 t_cylinder *cylinder = &scene->cylinders[hit.index];
-//                 normal = get_cylinder_normal(hit.point, cylinder);
-//                 if (cylinder->material.checker == 1)
-//                 {
-//                     int is_black = is_checkerboard(hit.point, cylinder, 0.5); // Scale = 1.0 for fine squares
-//                     t_color object_color = is_black ? black : white;
-//                     final_color = apply_lighting(hit.point, normal, object_color, scene, depth + 1);
-//                 }
-//                 else
-//                     final_color = apply_lighting(hit.point, normal, cylinder->material.color, scene, depth + 1);
-//                 hit.material.reflectivity = cylinder->material.reflectivity;
-//                 hit.material.transparency = cylinder->material.transparency;
-//                 hit.material.refractive_index = cylinder->material.refractive_index;
-//                 break;
-//             }
-//             case PLANE:
-//             {
-//                 t_plane *plane = &scene->planes[hit.index];
-//                 normal = plane->normal;
-//                 if (dot(ray.direction, normal) > 0)
-// 	    			normal = multiply_scalar(normal, -1); // Flip the normal
-//                 if (plane->material.checker == 1)
-//                 {
-//                     t_color object_color = get_plane_checkerboard_color(hit.point, black, white, normal, 0.5);
-//                     final_color = apply_lighting(hit.point, normal, object_color, scene, depth + 1);
-//                 }
-//                 else
-//                     final_color = apply_lighting(hit.point, normal, plane->material.color, scene, depth + 1);
-//                 hit.material.reflectivity = plane->material.reflectivity;
-//                 hit.material.transparency = plane->material.transparency;
-//                 hit.material.refractive_index = plane->material.refractive_index;
-//                 break;
-//             }
-//             case DISC:
-//             {
-//                 t_disc *disc = &scene->discs[hit.index];
-//                 normal = disc->normal;
-//     	        t_color gradient;
-//                 if (disc->material.checker == 1)
-//                 {
-//                     t_color object_color = get_disc_checkerboard_color(hit.point, disc, black, white, 0.5);
-//                     final_color = apply_lighting(hit.point, normal, object_color, scene, depth + 1);
-//                 }
-//                 else
-//                 {
-//                     gradient = apply_lighting(hit.point, normal, disc->color, scene, depth + 1);
-//                     gradient = apply_lighting(hit.point, normal, disc->color, scene, depth + 1);
-//               		final_color = gradient;
-//                 }
-//                 hit.material.reflectivity = disc->material.reflectivity;
-//                 hit.material.transparency = disc->material.transparency;
-//                 hit.material.refractive_index = disc->material.refractive_index;
-//                 break;
-//             }
-//         }
-//         if (hit.material.reflectivity > 0.0)
-//         {
-//             t_ray reflection_ray = get_reflection_ray(hit.point, normal, ray);
-//             reflection_ray.origin = add(reflection_ray.origin,
-//                                       multiply_scalar(reflection_ray.direction, 0.001));
-//             t_color reflected_color = trace_ray(reflection_ray, scene, depth + 1);
-//             final_color = blend_colors(final_color, reflected_color, hit.material.reflectivity);
-//         }
-//         if (hit.material.transparency > 0.0)
-//         {
-//             float eta_ratio = (hit.hit_from_inside) ? hit.material.refractive_index : (1.0 / hit.material.refractive_index);
-//             if (eta_ratio > 1.0)
-//             {
-//                 t_vector reflected_dir = reflect(ray.direction, hit.normal);
-//                 t_ray reflected_ray = {hit.point, reflected_dir};
-//                 reflected_ray.origin = add(reflected_ray.origin, 
-//                                            multiply_scalar(reflected_ray.direction, 0.001));
-//                 t_color reflected_color = trace_ray(reflected_ray, scene, depth + 1);
-//                 final_color = blend_colors(final_color, reflected_color, hit.material.transparency);
-//             }
-//             else
-//             {
-//                 t_vector refracted_dir = refract(ray.direction, hit.normal, eta_ratio);
-//                 t_ray refracted_ray = {hit.point, refracted_dir};
-//                 refracted_ray.origin = add(refracted_ray.origin, 
-//                                            multiply_scalar(refracted_ray.direction, 0.001));
-//                 t_color refracted_color = trace_ray(refracted_ray, scene, depth + 1);
-//                 final_color = blend_colors(final_color, refracted_color, hit.material.transparency);
-//             }
-//         }
-//     }
-//     return final_color;
-// }
-
 static t_ray get_refraction_ray(t_vector point, t_vector normal, t_ray incoming_ray, double refractive_index)
 {
     t_ray refraction_ray;
@@ -386,7 +178,6 @@ static t_ray get_refraction_ray(t_vector point, t_vector normal, t_ray incoming_
         refraction_ray.origin = point;
         refraction_ray.direction = normalize(refracted_dir);
     }
-    
     return refraction_ray;
 }
 
@@ -604,7 +395,7 @@ t_color trace_ray(t_ray ray, t_scene *scene, int depth)
     }
     return (black);
 }
-// trace_ray_utils.c
+
 void check_sphere_intersections(t_ray ray, t_scene *scene,     t_hit_record *hit)
 {
     int i;
@@ -689,8 +480,6 @@ void check_plane_intersections(t_ray ray, t_scene *scene,     t_hit_record *hit)
     }
 }
 
-
-
 t_color apply_reflection(t_material_params params, t_color base_color)
 {
     t_ray   reflection_ray;
@@ -741,56 +530,44 @@ double schlick_reflection_coefficient(double cos_theta, double refractive_index)
 // New function to handle rendering in threads
 void *render_thread(void *arg)
 {
-    // double reflectivity = 0.0;
     t_thread_data *thread_data = (t_thread_data *)arg;
     t_render_data *data = thread_data->render_data;
-    // t_color black = {0, 0, 0};
-    // t_color white = {255, 255, 255};
-    // t_vector normal;
-	// pthread_detach(pthread_self());
     int x;
     int y;
     int thread_id = thread_data->thread_id;
     int num_threads = thread_data->num_threads;
-y = 0;
-while (y < HEIGHT) {
-    x = 0;
-    while (x < WIDTH) {
-        int pixel_index = y * WIDTH + x;
-        if (pixel_index % num_threads == thread_id)
-        {
-            t_ray ray = create_ray(x, y, &data->scene->camera);
-            t_color final_color = trace_ray(ray, data->scene, 5);
-            // double t;
-            uint32_t color = (final_color.r << 24) | (final_color.g << 16) | (final_color.b << 8) | 0xFF;
-            pthread_mutex_lock(&data->mutex);
-            mlx_put_pixel(data->img, x, y, color);
-            pthread_mutex_unlock(&data->mutex);
-        }
-        x++;
-    }
-    y++;
-}
-
-    // Update thread completion count
+	y = 0;
+	while (y < HEIGHT)
+	{
+    	x = 0;
+    	while (x < WIDTH)
+		{
+      	  int pixel_index = y * WIDTH + x;
+        	if (pixel_index % num_threads == thread_id)
+        	{
+            	t_ray ray = create_ray(x, y, &data->scene->camera);
+            	t_color final_color = trace_ray(ray, data->scene, 5);
+            	uint32_t color = (final_color.r << 24) | (final_color.g << 16) | (final_color.b << 8) | 0xFF;
+            	pthread_mutex_lock(&data->mutex);
+            	mlx_put_pixel(data->img, x, y, color);
+            	pthread_mutex_unlock(&data->mutex);
+        	}
+        	x++;
+    	}
+    	y++;
+	}
     pthread_mutex_lock(&data->mutex);
     data->threads_completed++;
-    // If this is the last thread to complete
     if (data->threads_completed == NUM_THREADS)
     {
         gettimeofday(&data->end_time, NULL);
         double elapsed_time = (data->end_time.tv_sec - data->start_time.tv_sec) + 
                             (data->end_time.tv_usec - data->start_time.tv_usec) / 1e6;
         printf("Rendering took %f seconds\n", elapsed_time);
-        
-        // Save the image
         save_image_to_file(data->img->pixels, WIDTH, HEIGHT, "output.png");
-        
-        // Set the finished flag instead of exiting
         data->rendering_finished = 1;
     }
     pthread_mutex_unlock(&data->mutex);
-
     free(thread_data);
     return NULL;
 }
@@ -801,20 +578,6 @@ int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
     return (r << 24 | g << 16 | b << 8 | a);
 }
 
-// Function to create a ray from the camera for a specific pixel
-// t_ray create_ray(int x, int y, t_camera *camera)
-// {
-//     t_ray ray;
-//     ray.origin = camera->pos;
-
-//     // Calculate direction from camera to pixel (simple perspective projection)
-//     ray.direction.x = (2 * (x + 0.5) / (double)WIDTH - 1) * tan(camera->fov / 2 * M_PI / 180);
-//     ray.direction.y = (1 - 2 * (y + 0.5) / (double)HEIGHT) * tan(camera->fov / 2 * M_PI / 180);
-//     ray.direction.z = 1; // Assume camera is looking along the positive z-axis
-//     ray.direction = normalize(ray.direction);
-
-//     return ray;
-// }
 t_ray create_ray(int x, int y, t_camera *camera)
 {
     t_ray ray;
